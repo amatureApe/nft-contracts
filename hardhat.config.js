4b5630ff6924c3acc6add84f6f33dcdf0855da4e
amatureApe <amatureApe@gmail.com>
2022-06-30 21:32:04 -0500

    BasicNft mocks and tests built

--- hardhat.config.js
+++ hardhat.config.js
@@ -31,8 +31,14 @@ module.exports = {
     outputFile: "gas-report.txt",
     noColors: true,
   },
-  solidity: "0.8.7",
-  namedAccounts: {
+  solidity: {
+    compilers: [
+      { version: "0.8.7" },
+      { version: "0.6.6" },
+      { version: "0.6.12" },
+      { version: "0.4.19" },
+    ]
+  }, namedAccounts: {
     deployer: {
       default: 0
     },
