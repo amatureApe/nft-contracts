ae754360d84939a612efbd94e410b0bac9770ebd
amatureApe <amatureApe@gmail.com>
2022-07-01 11:35:49 -0500

    building RandomIpfsNft

---
+++ contracts/RandomIpfsNft.sol
@@ -0,0 +1,103 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+error RandomIpfsNft__RangeOutOfBounds();
+
+contract RandomIpfsNft is VRFConsumerBaseV2, ERC721URIStorage {
+    // when we mint an NFT, we will trigger the Chainlink VRF call to get us a random number
+    // from that number, we will get a random NFT with different rarities
+
+    // users have to pay to mint
+    // the owner of the contract can withdraw the ETH
+
+    // Type Declarations
+    enum Breed {
+        PUG,
+        SHIBA_INU,
+        ST_BERNARD
+    }
+
+    // Chainlink VRF variables
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    uint64 private immutable i_subscriptionId;
+    bytes32 private immutable i_gasLane;
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+
+    // VRF Helpers
+    mapping(uint256 => address) public s_requestIdToSender;
+
+    // NFT variables
+    uint256 public s_tokenCounter;
+    uint256 private constant MAX_CHANCE_VALUE = 100;
+    string[] internal s_dogTokenUris;
+
+    constructor(
+        address vrfCoordinatorV2,
+        uint64 subscriptionId,
+        bytes32 gasLane,
+        uint32 callbackGasLimit,
+        string[3] memory dogTokenUris
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) ERC721("Random IPFS", "RIN") {
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+        i_subscriptionId = subscriptionId;
+        i_gasLane = gasLane;
+        i_callbackGasLimit = callbackGasLimit;
+        s_dogTokenUris = dogTokenUris;
+    }
+
+    function requestNft() public returns (uint256 requestId) {
+        requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+        s_requestIdToSender[requestId] = msg.sender;
+    }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords)
+        internal
+        override
+    {
+        address dogOwner = s_requestIdToSender[requestId];
+        uint256 newTokenId = s_tokenCounter;
+        uint256 moddedRng = randomWords[0] % MAX_CHANCE_VALUE;
+
+        Breed dogBreed = getBreedFromModdedRng(moddedRng);
+        _safeMint(dogOwner, newTokenId);
+        _setTokenURI(newTokenId, s_dogTokenUris[uint256(dogBreed)]);
+    }
+
+    function getBreedFromModdedRng(uint256 moddedRng)
+        public
+        pure
+        returns (Breed)
+    {
+        uint256 cumulativeSum = 0;
+        uint256[3] memory chanceArray = getChanceArray();
+        for (uint256 i = 0; i < chanceArray.length; i++) {
+            if (
+                moddedRng >= cumulativeSum &&
+                moddedRng < cumulativeSum + chanceArray[i]
+            ) {
+                return Breed(i);
+            }
+            cumulativeSum += chanceArray[i];
+        }
+        revert RandomIpfsNft__RangeOutOfBounds();
+    }
+
+    function getChanceArray() public pure returns (uint256[3] memory) {
+        return [10, 30, MAX_CHANCE_VALUE];
+    }
+
+    function tokenURI(uint256) public view override returns (string memory) {}
+}
